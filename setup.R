164543a4561795fb822ce6d9db90a0333a83bf45
juanchiem <edwardsmolina@gmail.com>
2023-07-11 16:15:03 -0300

    render book full

--- setup.R
+++ setup.R
@@ -1,41 +1,33 @@
 library(tidyverse) 
-library(ggpmisc)
-library(ggstance)
+# library(ggpmisc)
+# library(ggstance)
 library(huxtable)
 # library(patchwork)
 library(lme4)
-library(nlme)
+library(lmerTest)
+library(car)
 library(emmeans)
 library(multcomp)
 library(performance)
+library(rio)
+library(skimr)
 
-conflicted::conflicts_prefer(ggplot2::annotate)
 conflicted::conflicts_prefer(dplyr::filter)
-conflicted::conflict_prefer("select", "dplyr")
-
+conflicted::conflicts_prefer(dplyr::select)
+conflicted::conflicts_prefer(dplyr::recode)
+conflicted::conflicts_prefer(lme4::lmer)
+conflicted::conflicts_prefer(broom::tidy)
 theme_set(theme_bw(base_size = 12)) 
-# knitr::opts_chunk$set(echo = F, warning = FALSE, message = FALSE)
 
-# asin_tran <- make.tran("asin.sqrt", 100)
-
+hux <- function(data, x) {
+  data %>%
+    as_hux() %>% 
+    theme_article() %>% 
+    set_font_size(9) %>%    
+    set_top_padding(.2) %>%
+    set_bottom_padding(.2) %>% 
+    set_number_format(x)
+}
 # quarto::quarto_render("document.qmd") # defaults to html
 # usethis::browse_github()
 # tinytex::install_tinytex(repository = "http://mirrors.tuna.tsinghua.edu.cn/CTAN/", version = "latest")
-
-# library(gistr)
-# gist_auth()
-# gists('minepublic', per_page = 2)
-
-# inputs
-# rinde_humedo <- 5000  # Rendimiento de grano en kg/ha
-# humedad <- 8
-# hum_recibo <- 11
-# porcentaje_aceite <- 45  # Porcentaje de aceite
-# bonif <- 42
-# 
-# # agua <- rinde_humedo*humedad/100
-# rinde_seco <- rinde_humedo - (rinde_humedo*humedad/100)
-# rinde_bonif <- rinde_seco * (1 + (porcentaje_aceite - 42) * 2/100)
-# rinde_bonif_recibo <-  rinde_bonif*(1+hum_recibo/100)
-# rinde_bonif_recibo
-# # 
